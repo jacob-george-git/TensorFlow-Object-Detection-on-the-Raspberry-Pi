cbb3e4b4cf73f359c4bd823d31bfd15a5f78f3dc
Jacob George <gjacob@gmail.com>
2021-08-13 09:58:52 -0700

    Changes for tensorflow 2.1

--- Object_detection_picamera.py
+++ Object_detection_picamera.py
@@ -16,7 +16,7 @@
 ## https://github.com/datitran/object_detector_app/blob/master/object_detection_app.py
 
 ## but I changed it to make it more understandable to me.
-
+# Jacob: Changes based on tensorflow 2.1 
 
 # Import packages
 import os
@@ -76,16 +76,17 @@ label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
 
+
 # Load the Tensorflow model into memory.
 detection_graph = tf.Graph()
 with detection_graph.as_default():
-    od_graph_def = tf.GraphDef()
-    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+    od_graph_def = tf.compat.v1.GraphDef()
+    with tf.io.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
         serialized_graph = fid.read()
         od_graph_def.ParseFromString(serialized_graph)
         tf.import_graph_def(od_graph_def, name='')
 
-    sess = tf.Session(graph=detection_graph)
+    sess = tf.compat.v1.Session(graph=detection_graph)
 
 
 # Define input and output tensors (i.e. data) for the object detection classifier
